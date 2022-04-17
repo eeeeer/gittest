697e9b626c3aaca4def9d5f5bacd742452cb5641
ylk <1534437818@qq.com>
2022-04-17 14:42:45 +0800

    22

--- src/main/java/git/p1_GitTest.java
+++ src/main/java/git/p1_GitTest.java
@@ -3,7 +3,7 @@ package git;
 public class p1_GitTest {
     public static void main(String[] args) {
         System.out.println("hello git ");
-        System.out.println("hello git2 ");
+        System.out.println("hello git2 master ");
         System.out.println("hello git3 ");
 
 
